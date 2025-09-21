fcdd6db15055c4791a44aed6d0b9c6c05b1d9b58
AlyaaRana <alyaaranaraya@gmail.com>
2025-09-21 18:35:26 +0700

    add data citylist

---
+++ app/data/CityList.js
@@ -0,0 +1,57 @@
+export const cityList = [
+  // === JAKARTA ===
+  { adm4: '31.71.03.1001', name: 'Kemayoran, Jakarta Pusat' },
+  { adm4: '31.71.01.1006', name: 'Duri Pulo, Gambir, Jakarta Pusat' },
+  { adm4: '31.71.08.1002', name: 'Kampung Rawa, Johar Baru, Jakarta Pusat' },
+  { adm4: '31.71.02.1001', name: 'Pasar Baru, Sawah Besar, Jakarta Pusat' },
+  { adm4: '31.72.01.1001', name: 'Penjaringan, Jakarta Utara' },
+
+  // === BANDUNG ===
+  { adm4: '32.73.05.1005', name: 'Cihapit, Bandung Wetan, Bandung' },
+  { adm4: '32.73.07.1001', name: 'Cicadas, Cibeunying Kidul, Bandung' },
+  { adm4: '32.73.04.1002', name: 'Sukamaju, Coblong, Bandung' },
+  { adm4: '32.73.02.1003', name: 'Kebon Pisang, Sumur Bandung, Bandung' },
+  { adm4: '32.73.06.1004', name: 'Cibangkong, Batununggal, Bandung' },
+
+  // === SEMARANG ===
+  { adm4: '33.74.01.1002', name: 'Brumbungan, Semarang Tengah' },
+  { adm4: '33.74.01.1004', name: 'Kranggan, Semarang Tengah' },
+  { adm4: '33.74.07.1004', name: 'Mugassari, Semarang Selatan' },
+  { adm4: '33.74.10.1001', name: 'Bulusan, Tembalang, Semarang' },
+  { adm4: '33.74.05.1003', name: 'Karangroto, Genuk, Semarang' },
+
+  // === SURABAYA ===
+  { adm4: '35.78.07.1004', name: 'Ketabang, Genteng Surabaya Pusat' },
+  { adm4: '35.78.08.1003', name: 'Airlangga, Gubeng, Surabaya Timur' },
+  { adm4: '35.78.14.1007', name: 'Balongsari, Tandes, Surabaya Barat' },
+  { adm4: '35.78.17.1003', name: 'Bulak Banteng, Kenjeran, Surabaya Utara' },
+  { adm4: '35.78.04.1003', name: 'Ngagel, Wonokromo, Surabaya Selatan' },
+
+  // Makasar
+  { adm4: '73.71.03.1001', name: 'Maricaya, Makassar' },
+  { adm4: '73.71.04.1008', name: 'Losari, Ujung Pandang, Makassar' },
+  { adm4: '73.71.05.1006', name: 'Mampu, Wajo, Makassar' },
+  { adm4: '73.71.09.1001', name: 'Karuwisi, Panakkukang, Makassar' },
+  { adm4: '73.71.13.1001', name: 'Tidung, Rappocini, Makassar' },
+
+  // Medan
+  { adm4: '12.71.19.1001', name: 'Petisah Tengah, Medan Petisah, Medan' },
+  { adm4: '12.71.15.1001', name: 'Aur, Medan Maimun, Medan' },
+  { adm4: '12.71.05.1003', name: 'Glugur Kota, Medan Barat, Medan' },
+  { adm4: '12.71.20.1003', name: 'Durian, Medan Timur, Medan' },
+
+  // Denpasar
+  { adm4: '51.71.01.1004', name: 'Panjer, Denpasar Selatan, Denpasar' },
+  { adm4: '51.71.02.1010', name: 'Dangin Puri, Denpasar Timur, Denpasar' },
+  { adm4: '51.71.03.1005', name: 'Dauh Puri, Denpasar Barat, Denpasar' },
+  { adm4: '51.71.04.1009', name: 'Peguyangan, Denpasar Utara, Denpasar' },
+
+  // Yogyakarta
+  { adm4: '34.71.10.1001', name: 'Ngupasan, Gondomanan, Yogyakarta' },
+  { adm4: '34.71.09.1002', name: 'Panembahan, Kraton, Yogyakarta' },
+  { adm4: '34.71.05.1002', name: 'Pringgokusuman, Gedongtengen, Yogyakarta' },
+  { adm4: '34.71.03.1004', name: 'Baciro, Gondokusuman, Yogyakarta' },
+
+  // === dst (Surabaya, Makassar, Medan, Denpasar, Yogyakarta) ===
+];
+
