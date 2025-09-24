417a0c80bffdce1731a722e3ec3d9fbf9a908b12
Kaori <hazzakao@gmail.com>
2025-09-24 16:57:51 +0700

    update city

--- data/CityList.ts
+++ data/CityList.ts
@@ -27,17 +27,35 @@ export const cityList: CityData[] = [
   { adm4: '33.74.05.1003', name: 'Karangroto, Genuk, Semarang' },
   
   // === SURABAYA ===
-  // { adm4: 'xxx', name: 'Area Surabaya' },
-  
-  // === MAKASSAR ===
-  // { adm4: 'xxx', name: 'Area Makassar' },
-  
-  // === MEDAN ===
-  // { adm4: 'xxx', name: 'Area Medan' },
-  
-  // === DENPASAR ===
-  // { adm4: 'xxx', name: 'Area Denpasar' },
-  
-  // === YOGYAKARTA ===
-  // { adm4: 'xxx', name: 'Area Yogyakarta' },
+  { adm4: '35.78.07.1004', name: 'Ketabang, Genteng Surabaya Pusat' },
+  { adm4: '35.78.08.1003', name: 'Airlangga, Gubeng, Surabaya Timur' },
+  { adm4: '35.78.14.1007', name: 'Balongsari, Tandes, Surabaya Barat' },
+  { adm4: '35.78.17.1003', name: 'Bulak Banteng, Kenjeran, Surabaya Utara' },
+  { adm4: '35.78.04.1003', name: 'Ngagel, Wonokromo, Surabaya Selatan' },
+
+  // Makasar
+  { adm4: '73.71.03.1001', name: 'Maricaya, Makassar' },
+  { adm4: '73.71.04.1008', name: 'Losari, Ujung Pandang, Makassar' },
+  { adm4: '73.71.05.1006', name: 'Mampu, Wajo, Makassar' },
+  { adm4: '73.71.09.1001', name: 'Karuwisi, Panakkukang, Makassar' },
+  { adm4: '73.71.13.1001', name: 'Tidung, Rappocini, Makassar' },
+
+  // Medan
+  { adm4: '12.71.19.1001', name: 'Petisah Tengah, Medan Petisah, Medan' },
+  { adm4: '12.71.15.1001', name: 'Aur, Medan Maimun, Medan' },
+  { adm4: '12.71.05.1003', name: 'Glugur Kota, Medan Barat, Medan' },
+  { adm4: '12.71.20.1003', name: 'Durian, Medan Timur, Medan' },
+
+  // Denpasar
+  { adm4: '51.71.01.1004', name: 'Panjer, Denpasar Selatan, Denpasar' },
+  { adm4: '51.71.02.1010', name: 'Dangin Puri, Denpasar Timur, Denpasar' },
+  { adm4: '51.71.03.1005', name: 'Dauh Puri, Denpasar Barat, Denpasar' },
+  { adm4: '51.71.04.1009', name: 'Peguyangan, Denpasar Utara, Denpasar' },
+
+  // Yogyakarta
+  { adm4: '34.71.10.1001', name: 'Ngupasan, Gondomanan, Yogyakarta' },
+  { adm4: '34.71.09.1002', name: 'Panembahan, Kraton, Yogyakarta' },
+  { adm4: '34.71.05.1002', name: 'Pringgokusuman, Gedongtengen, Yogyakarta' },
+  { adm4: '34.71.03.1004', name: 'Baciro, Gondokusuman, Yogyakarta' },
+
 ];
