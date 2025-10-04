048a8e7ab53db90c78bf4789ed1651988624e106
Kaori <hazzakao@gmail.com>
2025-10-04 23:13:55 +0700

    add AI

--- components/Dock.jsx
+++ components/Dock.jsx
@@ -36,17 +36,13 @@ export function FloatingDockDemo() {
       href: "/article",
     },
     {
-      title: "Aceternity UI",
+      title: "Artificial Intelligence",
       icon: (
-        <img
-          src="https://assets.aceternity.com/logo-dark.png"
-          width={20}
-          height={20}
-          alt="Aceternity Logo" 
-        />
+        <IconBook2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />
       ),
-      href: "#",
+      href: "/ai",
     },
+
     {
       title: "Profile",
       icon: (
